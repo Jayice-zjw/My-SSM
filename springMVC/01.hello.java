bbc1cb7cb0d4efb7c2f133c6f8f57ff78f49149b
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-14 19:45:38 +0800

    Update 01.hello.java

--- springMVC/01.hello.java
+++ springMVC/01.hello.java
@@ -6,17 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-//连接Controller接口
-
 public class hello implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ModelAndView mv = new ModelAndView();
-    //类似于request的setAtrribute方法
+		//相当于request的setAtrribute方法
 		mv.addObject("hello", "hello first spring mvc");
-    //指定要跳转的jsp界面
-		mv.setViewName("/WEB-INF/jsp/first.jsp");
+		mv.setViewName("first");  //要跳转的jsp
 		return mv;
 	}
 
