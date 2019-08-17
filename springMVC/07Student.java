90935f589fe40369c9863a1e3eeaa497da319167
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-17 14:38:27 +0800

    Create 07Student.java

---
+++ springMVC/07Student.java
@@ -0,0 +1,25 @@
+package com.monkey1024.controller;
+
+public class Student {
+	private String name;
+	private int age;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	private School school;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public School getSchool() {
+		return school;
+	}
+	public void setSchool(School school) {
+		this.school = school;
+	}
+}
