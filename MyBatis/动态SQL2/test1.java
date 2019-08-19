edaecd015ff8d0a02847626e11bb11dee6a9c741
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-19 15:55:24 +0800

    Create test1.java

---
+++ MyBatis/动态SQL2/test1.java
@@ -0,0 +1,54 @@
+package com.jay.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.jay.bean.Student;
+import com.jay.dao.*;
+import com.jay.util.util;
+
+public class test01 {
+
+	private studentDao studentDao = null;
+	private SqlSession sqlSession = null;
+
+	@Before
+	public void init() {
+		sqlSession = util.getSqlSession();
+		studentDao = sqlSession.getMapper(studentDao.class);
+	}
+
+	@Test
+	public void selectStudents1() {
+		Student student = new Student("杰", 30);
+		List<Student> res = studentDao.selectStudents(student);
+		res.forEach(r -> {
+			System.out.println(r);
+		});
+	}
+
+	@Test
+	public void selectStudents2() {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(4);
+		List<Student> res = studentDao.selectStudents2(list);
+		res.forEach(r -> {
+			System.out.println(r);
+		});
+	}
+
+	@After
+	public void close() {
+		if (sqlSession != null) {
+			sqlSession.close();
+		}
+	}
+
+}
