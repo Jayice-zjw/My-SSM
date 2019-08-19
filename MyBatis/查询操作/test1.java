07625e9e6ef56d2fcf97fcb8a9fb5809fea9855d
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-19 13:19:10 +0800

    Create test1.java

---
+++ MyBatis/查询操作/test1.java
@@ -0,0 +1,48 @@
+package com.jay.test;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.jay.bean.Student;
+import com.jay.dao.studentDao;
+import com.jay.dao.impl.studentDaoImpl;
+
+public class test01 {
+
+	private studentDao studentDao;
+
+	@Test
+	public void insertStudent() {
+		studentDao = new studentDaoImpl();
+		Student student = new Student("陈奕迅", 43, 97.00);
+
+		studentDao.insertStudent(student);
+	}
+
+	@Test
+	public void deleteStudent() {
+		studentDao = new studentDaoImpl();
+		studentDao.deleteStudent(5);
+	}
+	
+	@Test
+	public void updateStudent() {
+		Student student=new Student("周杰伦",40,100);
+		student.setId(1);
+		studentDao = new studentDaoImpl();
+		studentDao.updateStudent(student);
+		
+	}
+	
+	@Test
+	public void selectAllStudent() {
+		studentDao = new studentDaoImpl();
+		List<Student> res=studentDao.selectAllStudent();
+		for(Student s:res) {
+			System.out.print(s.getId());
+			System.out.print(s.getName());
+			System.out.println();
+		}
+	}
+}
