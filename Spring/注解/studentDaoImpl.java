2fa8a68777d69a555f811988413709359cd373a2
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-21 00:19:26 +0800

    Update studentDaoImpl.java

--- Spring/注解/studentDaoImpl.java
+++ Spring/注解/studentDaoImpl.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Repository;
 
 import com.jay.dao1.student;
 
-@Repository("student")
+@Repository("studentDao1")
 public class studentImpl implements student {
 
 	@Override
