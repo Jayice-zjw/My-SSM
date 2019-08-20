316459ac59ae13b72df6df0a767fd282754f5967
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-20 23:08:36 +0800

    Create test.java

---
+++ Spring/bean的生命周期/test.java
@@ -0,0 +1,42 @@
+package com.jay.test;
+
+import com.jay.service1.StudentService;
+import com.jay.service.impl.StudentServiceImpl;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test1 {
+
+	/**
+	 * 以前的写法：手动创建对象
+	 */
+	@Test
+	public void oldType() {
+		StudentService studentService = new StudentServiceImpl();
+		studentService.study();
+	}
+
+	/**
+	 * 使用spring之后的写法：直接通过spring获取对象
+	 */
+	@Test
+	public void springType() {
+		// 读取spring配置文件
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		// 从spring中获取对象
+		StudentService studentService = (StudentService) context.getBean("studentService");
+		studentService.study();
+		
+		//context是ApplicationContext类型的，需要强转回去
+		((ClassPathXmlApplicationContext)context).close();
+		/*
+	 	构造器
+		bean的before方法
+		bean初始化
+		bean的After方法
+		好好学习
+		bean销毁
+		 * */
+	}
+}
