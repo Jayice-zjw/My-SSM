0a28403a212e6c90bb8733bf921edbf8febf4c80
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-20 21:43:41 +0800

    Create StudentServiceImpl.java

---
+++ Spring/第一个Spring/src.main.java/StudentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.jay.service.impl;
+
+import com.jay.service1.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public void study() {
+		System.out.println("好好学习");
+		
+	}
+
+}
