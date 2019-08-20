ad27cbe3ed3eae12c071ba4fb83d6e883925bb6f
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-21 00:21:33 +0800

    Update studentServiceImpl.java

--- Spring/注解/studentServiceImpl.java
+++ Spring/注解/studentServiceImpl.java
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import com.jay.service.impl.studentService;
 
+
+//Autowired和Resource的区别。Autowired由Spring提供，默认根据类型装配，而Resource又java提供，默认根据名字进行装配
 @Repository("studentService")
 public class studentServiceImpl implements studentService {
 	@Autowired
+	@Resource("studentDao1")
 	studentService sd;
 	
 	@Override
