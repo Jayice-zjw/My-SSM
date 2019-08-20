fb5366daab50a9935d8a737e238b0496cc5cd4c6
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-20 22:29:37 +0800

    Create test(单例).java

---
+++ Spring/bean的作用域/test(单例).java
@@ -0,0 +1,32 @@
+package com.jay.test;
+
+import com.jay.service1.StudentService;
+import com.jay.service.impl.StudentServiceImpl;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test1 {
+
+	/**
+	 * 以前的写法：手动创建对象
+	 */
+	@Test
+	public void oldType() {
+		StudentService studentService = new StudentServiceImpl();
+		studentService.study();
+	}
+
+	/**
+	 * 使用spring之后的写法：直接通过spring获取对象
+	 */
+	@Test
+	public void springType() {
+		// 读取spring配置文件
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		// 从spring中获取对象
+		StudentService studentService = (StudentService) context.getBean("studentService");
+		StudentService studentService1 = (StudentService) context.getBean("studentService");
+		System.out.println(studentService == studentService1);  //true
+	}
+}
