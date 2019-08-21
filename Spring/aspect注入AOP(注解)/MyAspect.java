e24fdec49869eb8f96ccc4148949ed0aa6eadf81
JAYICE <49588871+1185430411@users.noreply.github.com>
2019-08-21 22:05:40 +0800

    Create MyAspect.java

---
+++ Spring/aspect注入AOP(注解)/MyAspect.java
@@ -0,0 +1,39 @@
+package com.jay.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyAspect {
+
+	@Before("execution(* *..UserServiceImpl.addUser())")
+	public void before() {
+		System.out.println("========前置通知========");
+	}
+
+	@After("execution(* *..UserServiceImpl.selectUser())")
+	public void after() {
+		System.out.println("========最终通知========:");
+	}
+
+	@AfterThrowing(value = "execution(* *..UserServiceImpl.selectUserById(..))", throwing = "e")
+	public void afterThrowing(Exception e) {
+		System.out.println("========异常通知========:" + e);
+	}
+
+	@AfterReturning(value = "execution(* *..UserServiceImpl.updateUser())", returning = "result")
+	public void afterReturning(int result) {
+		System.out.println("========后置通知========:" + result);
+	}
+
+	@Around(value = "execution(* *..UserServiceImpl.deleteUser())")
+	public Object around(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("========环绕通知:前========:");
+		Object proceed = pjp.proceed();
+		System.out.println("========环绕通知:后========:");
+
+		return proceed;
+	}
+}
